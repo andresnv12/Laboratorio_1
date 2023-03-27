b22d6677643e7442e8ce54c5171e9c09daf6f9fd
andresnv12 <avargas1424@gmail.com>
2023-03-27 09:22:14 -0600

    Actualización de Código

--- Bingo_B92198.c
+++ Bingo_B92198.c
@@ -1,12 +1,10 @@
-
-#include <12f683.h>
+#include <pic14/pic12f683.h>
 #include <stdbool.h>
  
-
 typedef unsigned int word;
-// word __at 0x2007 _CONFIG = (WDT_OFF);
-int numeros[10]= (0b0000, 0b0001, 0b0010, 0b0011, 0b0100, 0b0101, 0b0110, 0b0111, 0b1000, 0b1001);
-int guardados[16] = (0);
+//word __at 0x2007 __CONFIG = (_WDT_OFF);
+unsigned short numeros[10]= {0b0000, 0b0001, 0b0010, 0b0011, 0b0100, 0b0101, 0b0110, 0b0111, 0b1000, 0b1001};
+unsigned short guardados[16] = {0};
 int n = 0;
 int NumDisplay = 0;
 void pantallas();
@@ -16,14 +14,13 @@ void main(void)
 {
     TRISIO = 0x00;
     GPIO = 0x00;
-    int GPIO3;
     while (true)
     {
         NumDisplay = 0; //Se encarga de volver el contador a cero para reiniciar la cuenta
         while(NumDisplay < 100)
         {
             RetenerPantalla();
-            if (GPIO3 = 1)
+            if (GP3 = ~GP3)
             {
                 Guardar();
             }
@@ -37,11 +34,11 @@ void pantallas()
     unidades = NumDisplay % 10;
     decenas = NumDisplay / 10;
 
-    int GPIO = numeros[unidades];
-    int GPIO5 = 0x00;
+    GPIO = numeros[unidades];
+    GP5 = 0;
     
     GPIO = numeros[decenas];
-    GPIO5 = 0x01;
+    GP5 = 1;
 }
 
 void RetenerPantalla()
@@ -57,10 +54,11 @@ void RetenerPantalla()
 
 void Guardar()
 {
+    int repetido = false;
+    int i;
     if (n < 16)
     {
-        int repetido = false;
-        int i;
+        
         for (i=0;i<16;i++)
         {
             if (NumDisplay = guardados[i])
@@ -70,7 +68,7 @@ void Guardar()
         }
         if (repetido = false)
         {
-            NumDisplay = guardados[n];
+            guardados[n] = NumDisplay;
             n++;
             RetenerPantalla();
         }
