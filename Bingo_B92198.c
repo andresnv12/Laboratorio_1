8f44c49ada0743cc7e93f3c8903337474de8b8fc
andresnv12 <avargas1424@gmail.com>
2023-03-27 20:22:01 -0600

    Cambios en el flujo de las funciones. Se añadió la función delay. Versión con problemas en los pines.

--- Bingo_B92198.c
+++ Bingo_B92198.c
@@ -1,18 +1,17 @@
 #include <pic14/pic12f683.h>
  
 typedef unsigned int word;
-//word __at 0x2007 __CONFIG = (_WDT_OFF);
-unsigned short numeros[10]= {0b0000, 0b0001, 0b0010, 0b0011, 0b0100, 0b0101, 0b0110, 0b0111, 0b1000, 0b1001};
+word __at 0x2007 __CONFIG = (_WDT_OFF & _MCLRE_OFF);
+unsigned short numeros[9]= {0b00000, 0b00001, 0b00010, 0b00011, 0b00100, 0b00101, 0b00110, 0b00111, 0b10000, 0b10001};
 unsigned short guardados[5] = {0};
 unsigned int n = 0;
-unsigned int NumDisplay = 0;
+unsigned int NumDisplay;
 void pantallas();
 void RetenerPantalla();
 void Guardar();
-
+void delay(unsigned int tiempo);
 void main(void)
 {
-    ANSEL = 0b00000010;
     TRISIO = 0b00000000; //todas los pines como salida
     GPIO = 0b00000000; //todos los pines en cero
     while (1)
@@ -21,32 +20,33 @@ void main(void)
         while(NumDisplay < 100) 
         {
             RetenerPantalla();
-            if (GP3 = 0x01)
+            if (GP3 = 1)
             {
                 Guardar();
-                GP3 = ~GP3;
+                
             }
             NumDisplay++; //contador y numero que se debe mostrar en pantalla
+            delay(100);
         }
     }
 }
-void pantallas()
+void pantallas() // Define los valores que se mostraran en la pantalla.
 {
     int unidades, decenas;
     unidades = NumDisplay % 10;
     decenas = NumDisplay / 10;
-    GP5 = 0;
-    GPIO = numeros[unidades];
 
-    
+    GPIO = numeros[unidades];
+    delay(1);
     GP5 = 1;
+    delay(1);
     GPIO = numeros[decenas];
-    
+    GP5 = 0;    
 }
 
-void RetenerPantalla()
+void RetenerPantalla() //Realiza una accion similar a delay. Se encarga de mantener los valores en pantalla
 {
-    int tiemporest = 500;
+    int tiemporest = 1000;
     while (tiemporest>0)
     {
         pantallas();
@@ -55,7 +55,7 @@ void RetenerPantalla()
     }
 }
 
-void Guardar()
+void Guardar() //Se encarga de verificar si NumDisplay es igual a un numero guardado cuando se pulsa el boton.
 {
     int repetido = 0;
     int i;
@@ -80,8 +80,15 @@ void Guardar()
     {
         n = 0;
         NumDisplay = 98;
-    }
-    
-    
+    }  
+}
+
+void delay(unsigned int tiempo) //Se crea un tiempo de espera
+{
+    unsigned int i ;
+    unsigned int j ;
+
+    for( i=0; i <tiempo; i ++)
+        for( j =0; j <1000; j ++);
 }
 
